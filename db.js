34cd77e98ffc6f054258f03f709f606f292b93dd
Egor Bondar <erop_96_15@mail.ru>
2023-10-29 22:18:28 +1000

    some

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const Pool = require('pg').Pool
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    user:'postgres',
-    password:'root',
-    host:'localhost',
-    port:'5432',
-    database:"node_postgres"
-})
+  user: 'zliwjhcf',
+  host: 'suleiman.db.elephantsql.com',
+  database: 'zliwjhcf',
+  password: '0DsTnHrkh7hfC0ECnJPBCLxnOMSY42FL', // Здесь укажите ваш пароль
+  port: 5432, // По умолчанию порт PostgreSQL 5432
+});
 
-module.exports = pool
+module.exports = pool;

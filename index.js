3eb6c58368a73781fa67d0a7295be4561ba0aa59
Egor Bondar <erop_96_15@mail.ru>
2023-10-30 13:33:56 +1000

    Добавил cors для доступа к серверу

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express =  require("express")
+const cors = require("cors"); // Подключаем cors
 
 const userRouter = require('./routes/user_routes')
 const postRouter = require('./routes/post_routes')
@@ -6,9 +7,11 @@ const postRouter = require('./routes/post_routes')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(cors());
+
 app.use(express.json())
 app.use('/api',userRouter)
-
 app.use('/api',postRouter)
 
 app.listen(PORT, () => console.log('SERVER STARTED ON ' + PORT))
